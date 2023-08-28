802e8820137ca3b0bcba5bb916f249963f47c521
Nkwor Jane <nkwor.jane.chinelo@gmail.com>
2023-08-27 17:05:34 -0700

    Making corrections

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
-# CLient configuration with Puppet
+# Client configuration with Puppet
 # Puppet is a tool that helps you manage and automate the configuration of server
-file {'etc/ssh/ssh_config':
+file {"/etc/ssh/ssh_config":
 	content => 'PassworAuthentication no
 	IdentityFile ~/.ssh/holberton',
 }
